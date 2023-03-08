8063b8c10658996479afb99e31bbb4372d12e5c0
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-08 03:25:32 +0300

    added finding a string in a file

--- dz1/src/main.cpp
+++ dz1/src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 int main()
 {
-    std::cout << "Hello world!";
+    std::string artist_name = "Robert De Niro";
+    std::string file_name = "../name.basics.tsv";
+    std::ifstream myfile; 
+    std::string name;
+    std::string temp;
+    myfile.open(file_name);
+    while (name.size() == 0 && !myfile.eof())
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            std::getline(myfile, temp, '\t');
+        }
+        if (temp == artist_name)
+            name = temp;
+        std::getline(myfile, temp);
+    }
+    std::cout << name << std::endl;
+    //std::cout << temp;
     return 0;
 }
