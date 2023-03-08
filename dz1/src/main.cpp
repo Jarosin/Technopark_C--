a924709df44cb52aa9ace5c23eeceb5e5e892718
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-08 04:37:35 +0300

    looking for russian name

--- dz1/src/main.cpp
+++ dz1/src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <locale> 
 std::string FindFilms(std::string file_name, std::string artist_name)
 {
     std::ifstream myfile;
@@ -41,16 +42,83 @@ std::vector<std::string> ParseFilmIdString(std::string film_ids)
     }
     return res;
 }
+bool isRussian(std::string name)
+{
+    //я пытался сделать по человечески
+    /*std::locale loc("ru-RU");
+    for (auto it = name.begin(); it < name.end(); it++)
+        if (!(std::isalpha(*it, loc) || std::isdigit(*it)))
+            return 0;*/
+    bool flag = false;
+    std::string rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    for (auto i = name.begin(); i < name.end(); i++)
+    {
+        if (std::isdigit(*i))
+            continue;
+        for (auto it = rus.begin(); it < rus.end(); it++)
+            if (*it == *i)
+            {
+                flag = true;
+                break;
+            }
+        if (!flag)
+            return false;
+        flag = false;
+    }
+    return true;
+}
+void FindFilmNames(std::string file_name, std::vector<std::string> films)
+{
+    std::ifstream myfile;
+    myfile.open(file_name);
+    std::string name;
+    std::string temp;
+    while (name.length() == 0 && !myfile.eof())
+    {
+        std::getline(myfile, temp, '\t');
+        for (auto it = films.begin(); it < films.end(); it++)
+        {
+            if ((*it) == temp)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    std::getline(myfile, temp, '\t');
+                }
+                *it = temp;
+                std::cout << "First name: " << temp << std::endl;
+                std::getline(myfile, temp, '\t');
+                std::cout << "Second name: " << temp << std::endl;
+                if (!isRussian(*it))
+                {
+                    if (isRussian(temp))
+                        *it = temp;
+                }
+                std::getline(myfile, temp, '\t');
+                if (temp == "1")
+                    *it = "";
+                break;
+            }
+        }
+        std::getline(myfile, temp, '\n');  
+    }
+}
+
 int main()
 {
     std::string artist_name = "Anya Chipovskaya";
-    std::string file_name = "../name.basics.tsv";
+    std::string nfile_name = "../name.basics.tsv";
+    std::string ffile_name = "../title.basics.tsv";
     std::string raw_films;
     std::vector<std::string> parsed_films;
     std::ifstream myfile; 
-    raw_films = FindFilms(file_name, artist_name);
+    raw_films = FindFilms(nfile_name, artist_name);
     std::cout << raw_films << std::endl;
     parsed_films = ParseFilmIdString(raw_films);
+    for (auto it = parsed_films.begin(); it < parsed_films.end(); it++)
+    {
+        std::cout << (*it) << std::endl;
+    }
+    FindFilmNames(ffile_name, parsed_films);
     for (auto it = parsed_films.begin(); it < parsed_films.end(); it++)
     {
         std::cout << (*it) << std::endl;
