8b93f492a2105981ae625a901b4e04c320dff0a0
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-08 04:39:11 +0300

    not looking for russian name

--- dz1/src/main.cpp
+++ dz1/src/main.cpp
@@ -42,32 +42,7 @@ std::vector<std::string> ParseFilmIdString(std::string film_ids)
     }
     return res;
 }
-bool isRussian(std::string name)
-{
-    //я пытался сделать по человечески
-    /*std::locale loc("ru-RU");
-    for (auto it = name.begin(); it < name.end(); it++)
-        if (!(std::isalpha(*it, loc) || std::isdigit(*it)))
-            return 0;*/
-    bool flag = false;
-    std::string rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    for (auto i = name.begin(); i < name.end(); i++)
-    {
-        if (std::isdigit(*i))
-            continue;
-        for (auto it = rus.begin(); it < rus.end(); it++)
-            if (*it == *i)
-            {
-                flag = true;
-                break;
-            }
-        if (!flag)
-            return false;
-        flag = false;
-    }
-    return true;
-}
-void FindFilmNames(std::string file_name, std::vector<std::string> films)
+void FindFilmNames(std::string file_name, std::vector<std::string> &films)
 {
     std::ifstream myfile;
     myfile.open(file_name);
@@ -80,19 +55,11 @@ void FindFilmNames(std::string file_name, std::vector<std::string> films)
         {
             if ((*it) == temp)
             {
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < 3; i++)
                 {
                     std::getline(myfile, temp, '\t');
                 }
                 *it = temp;
-                std::cout << "First name: " << temp << std::endl;
-                std::getline(myfile, temp, '\t');
-                std::cout << "Second name: " << temp << std::endl;
-                if (!isRussian(*it))
-                {
-                    if (isRussian(temp))
-                        *it = temp;
-                }
                 std::getline(myfile, temp, '\t');
                 if (temp == "1")
                     *it = "";
@@ -112,12 +79,7 @@ int main()
     std::vector<std::string> parsed_films;
     std::ifstream myfile; 
     raw_films = FindFilms(nfile_name, artist_name);
-    std::cout << raw_films << std::endl;
     parsed_films = ParseFilmIdString(raw_films);
-    for (auto it = parsed_films.begin(); it < parsed_films.end(); it++)
-    {
-        std::cout << (*it) << std::endl;
-    }
     FindFilmNames(ffile_name, parsed_films);
     for (auto it = parsed_films.begin(); it < parsed_films.end(); it++)
     {
