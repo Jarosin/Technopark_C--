ca2c4416f352b777aac0f551c4d08f6f1e8a9194
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-23 21:20:03 +0300

    add language check

--- dz1/src/main.cpp
+++ dz1/src/main.cpp
@@ -4,6 +4,24 @@
 #include <vector>
 #include <sstream>
 #include <locale> 
+#include <getopt.h>
+bool isRussian(std::string name)
+{
+    //я пытался сделать по человечески
+    /*std::locale loc("ru-RU");
+    for (auto it = name.begin(); it < name.end(); it++)
+        if (!(std::isalpha(*it, loc) || std::isdigit(*it)))
+            return 0;*/
+    std::string rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ,;':.!?<>[]{}";
+    for (auto i = name.begin(); i < name.end(); i++)
+    {
+        if (std::isdigit(*i))
+            continue;
+        if (rus.find(*i) == std::string::npos)
+            return false;
+    }
+    return true;
+}
 std::string FindFilms(std::string file_name, std::string artist_name)
 {
     std::ifstream myfile;
@@ -68,11 +86,14 @@ void FindFilmNames(std::string file_name, std::vector<std::string> &films, std::
                     *it = "";
                     break;
                 }
-                for (int i = 0; i < 2; i++)
-                {
-                    std::getline(myfile, temp, '\t');
-                }
+
+                std::getline(myfile, temp, '\t');
                 *it = temp;
+
+                std::getline(myfile, temp, '\t');
+                if (isRussian(temp))
+                    *it = temp;
+
                 std::getline(myfile, temp, '\t');
                 if (temp == "1")
                     *it = "";
@@ -83,33 +104,70 @@ void FindFilmNames(std::string file_name, std::vector<std::string> &films, std::
     }
     myfile.close();
 }
+int ParseArguments(int argc, char *argv[], std::string &name_file_name, std::string &title_file_name, std::string &artist_name)
+{
+    int opt;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"title-basics-file", 1, 0, 't'},
+        {"name-basics-file", 1, 0, 'n'},
+        {"help", 0, 0, 'h'},
+        {"artist-name", 1, 0, 'a'},
+        {0, 0, 0, 0}
+    };
+     while ((opt = getopt_long_only(argc, argv, "abc", long_options, &option_index)) != -1)
+    {
+        switch(opt)
+        {
+            case -1:
+                break;
+            case 't':
+                title_file_name = optarg;
+                break;
+            case 'n':
+                name_file_name = optarg;
+                break;
+            case 'a':
+                artist_name = optarg;
+                break;
+            case 'h':
+                std::cout << "Required arguments: --name-basics-file=filename1 --title-basics-file=filename2 --artist-name=artist_name" << std::endl;
+                return 1;
+                break;
+            default:
+                std::cout << "Unknown option given" << std::endl;
+                return 1;
+        }
+    }
+    if (title_file_name.empty() || name_file_name.empty() || artist_name.empty())
+    {
+        std::cout << "Not all arguments were given" << std::endl;
+        return 1;
+    }
+    return 0;
+}
 //1 - файл с актерами, 2 - файл с фильмами, 3 - имя актера
 int main(int argc, char *argv[])
-{
-    if (argc != 4)
-    {
-        return 0;
-    } 
-    std::string nfile_name = argv[1];
-    std::string ffile_name = argv[2];
-    std::string artist_name = argv[3];
+{    
+    std::string name_file_name = "";
+    std::string title_file_name = "";
+    std::string artist_name = "";
+    int rc = ParseArguments(argc, argv, name_file_name, title_file_name, artist_name);
+    if (rc)
+        return rc;
     std::string raw_films;
     std::vector<std::string> parsed_films;
     std::ifstream myfile; 
-    raw_films = FindFilms(nfile_name, artist_name);
+    raw_films = FindFilms(name_file_name, artist_name);
     parsed_films = ParseFilmIdString(raw_films);   
     std::vector<bool> checked_films(parsed_films.size(), false);
-    FindFilmNames(ffile_name, parsed_films, checked_films);
+    FindFilmNames(title_file_name, parsed_films, checked_films);
     for (auto it = parsed_films.begin(); it < parsed_films.end(); it++)
     {
-        if (checked_films[it - parsed_films.begin()])
+        if (checked_films[it - parsed_films.begin()] && *it != "")
             std::cout << (*it) << std::endl;    
     }
     return 0;
 }
-//todo: можно переорганизовать ввод под do-while
-//todo: проверить, что выбранные записи - movies
-//todo: посмотреть что с языком
-//todo: с помощью getopt переделать прием аргументов через "опции"
 //todo: разнести функции по файлам
 //todo: проверить что в документах правильное количество столбцов(насчет названий не уверен)
