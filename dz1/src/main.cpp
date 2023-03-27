ba83e643c970bd7ab16abbf20caad0c80ff09e02
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-27 22:52:05 +0300

    change error catching mechanism

--- dz1/src/main.cpp
+++ dz1/src/main.cpp
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
     std::string title_akas_file_name = "";
 
     //проверка валидности входных данных
-    int rc = ParseArguments(argc, argv, name_file_name, title_file_name, title_akas_file_name, actor_name);
-    if (!rc)
+    int rc;
+    if (rc = ParseArguments(argc, argv, name_file_name, title_file_name, title_akas_file_name, actor_name))
     {
-        rc = CheckDocument(name_file_name, 6);
-        if (!rc)
-        {
-            rc = CheckDocument(title_file_name, 9);
-            if (!rc)
-            {
-                rc = CheckDocument(title_akas_file_name, 8);
-            }
-        }
+        return rc;
     }
-    if (rc)
+    if (rc = CheckDocument(name_file_name, 6))
+    {
+        return rc;
+    }
+    if (rc = CheckDocument(title_file_name, 9))
+    {
+        return rc;
+    }
+    if (rc = CheckDocument(title_akas_file_name, 8))
     {
         return rc;
     }
@@ -38,22 +38,22 @@ int main(int argc, char *argv[])
     std::vector<std::string> film_names;
     
     //находим id фильмов
-    rc = FindFilmIds(name_file, actor_name, film_names);
-    if (!rc)
+    if (rc = FindFilmIds(name_file, actor_name, film_names))
     {
-        std::vector<bool> checked_films(film_names.size(), false);
-        std::vector<std::string> russian_titles = film_names;
-        CheckRus(title_akas_file, russian_titles);
-        //находим имена фильмов по id и чистим неподходящие по критериям
-        FindFilmNames(title_file, film_names, checked_films); 
-        rc = ClearFilmNames(film_names, russian_titles, checked_films);
-        if (!rc)
-        {
-            for (auto elm : film_names)
-            {
-                std::cout << elm << std::endl;
-            }
-        }
+        return rc;
+    }
+    std::vector<bool> checked_films(film_names.size(), false);
+    std::vector<std::string> russian_titles = film_names;
+    CheckRus(title_akas_file, russian_titles);
+    //находим имена фильмов по id и чистим неподходящие по критериям
+    FindFilmNames(title_file, film_names, checked_films); 
+    if (rc = ClearFilmNames(film_names, russian_titles, checked_films))
+    {
+        return rc;
+    }
+    for (auto elm : film_names)
+    {
+        std::cout << elm << std::endl;
     }
     name_file.close();
     title_file.close();
