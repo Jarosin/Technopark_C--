44d35e06665313e2a1164c3765e652f235e054b5
Spirin Mihail <misha_spirin_90@mail.ru>
2023-04-04 17:57:42 +0300

    add ceil

---
+++ dz2/Ceil.cpp
@@ -0,0 +1,3 @@
+#include "Ceil.hpp"
+#include <cmath>
+double Ceil::Calculate() { return ceil(var_->Calculate()); }
---
+++ dz2/Ceil.hpp
@@ -0,0 +1,17 @@
+#ifndef CEIL_H
+#define CEIL_H
+
+#pragma once
+
+#include "ICalculatable.hpp"
+#include "ICalculatableUnary.hpp"
+#include "memory"
+class Ceil : public ICalculatableUnary {
+ public:
+  using ICalculatableUnary::ICalculatableUnary;
+  ~Ceil() = default;
+  double Calculate() override;
+
+ private:
+};
+#endif
