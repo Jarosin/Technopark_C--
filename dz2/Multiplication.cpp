6b8d0debecbc0ff344502ca10e8ae1f59da9a168
Spirin Mihail <misha_spirin_90@mail.ru>
2023-04-04 17:48:30 +0300

    add multiplication

---
+++ dz2/Multiplication.cpp
@@ -0,0 +1,4 @@
+#include "Multiplication.hpp"
+
+double Multiplication::Calculate() { return var1_->Calculate() + var2_->Calculate(); }
+
---
+++ dz2/Multiplication.hpp
@@ -0,0 +1,18 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+
+#pragma once
+
+#include "ICalculatable.hpp"
+#include "ICalculatableBinary.hpp"
+#include "memory"
+using uptr_icalc = std::unique_ptr<ICalculatable>;
+class Multiplication : public ICalculatableBinary {
+ public:
+  using ICalculatableBinary::ICalculatableBinary;
+  ~Multiplication() = default;
+  double Calculate() override;
+};
+
+
+#endif
